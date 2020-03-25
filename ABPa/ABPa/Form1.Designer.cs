4525c5ab9b3c45086b52ae728e496d41dfeb2833
Artem Pankrat <artem-artem-68@bk.ru>
2020-03-25 11:13:51 +0300

    qwe

--- ABPa/ABPa/Form1.Designer.cs
+++ ABPa/ABPa/Form1.Designer.cs
@@ -32,14 +32,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxDron = new System.Windows.Forms.TextBox();
+            this.textBoxParts = new System.Windows.Forms.TextBox();
+            this.textBoxTech = new System.Windows.Forms.TextBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.openButtonTech = new System.Windows.Forms.Button();
+            this.openButtonParts = new System.Windows.Forms.Button();
             this.openButtonDron = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -79,43 +79,43 @@
             this.label4.TabIndex = 3;
             this.label4.Text = "Тех. карты";
             // 
-            // textBox1
+            // textBoxDron
             // 
-            this.textBox1.Location = new System.Drawing.Point(122, 53);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(192, 20);
-            this.textBox1.TabIndex = 4;
+            this.textBoxDron.Location = new System.Drawing.Point(122, 53);
+            this.textBoxDron.Name = "textBoxDron";
+            this.textBoxDron.Size = new System.Drawing.Size(192, 20);
+            this.textBoxDron.TabIndex = 4;
             // 
-            // textBox2
+            // textBoxParts
             // 
-            this.textBox2.Location = new System.Drawing.Point(122, 99);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(192, 20);
-            this.textBox2.TabIndex = 5;
+            this.textBoxParts.Location = new System.Drawing.Point(122, 99);
+            this.textBoxParts.Name = "textBoxParts";
+            this.textBoxParts.Size = new System.Drawing.Size(192, 20);
+            this.textBoxParts.TabIndex = 5;
             // 
-            // textBox3
+            // textBoxTech
             // 
-            this.textBox3.Location = new System.Drawing.Point(122, 147);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(192, 20);
-            this.textBox3.TabIndex = 6;
+            this.textBoxTech.Location = new System.Drawing.Point(122, 147);
+            this.textBoxTech.Name = "textBoxTech";
+            this.textBoxTech.Size = new System.Drawing.Size(192, 20);
+            this.textBoxTech.TabIndex = 6;
             // 
-            // button1
+            // downloadButton
             // 
             this.button1.Location = new System.Drawing.Point(111, 182);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 7;
-            this.button1.Text = "Загрузить";
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // openButtonParts
             // 
             this.button2.Location = new System.Drawing.Point(206, 182);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 8;
-            this.button2.Text = "Закрыть";
+            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // openButtonDron
@@ -132,50 +132,21 @@
             this.openButtonDron.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.openButtonDron.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.openButtonDron.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Image = global::ABPa.Properties.Resources.iconFolder;
-            this.button3.Location = new System.Drawing.Point(313, 99);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(43, 20);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "...";
-            this.button3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.Image = global::ABPa.Properties.Resources.iconFolder;
-            this.button4.Location = new System.Drawing.Point(313, 147);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(43, 20);
-            this.button4.TabIndex = 11;
-            this.button4.Text = "...";
-            this.button4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button4.UseVisualStyleBackColor = true;
+            this.openButtonDron.Click += new System.EventHandler(this.openButtonDron_Click);
             // 
             // downloadForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(415, 217);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.openButtonTech);
+            this.Controls.Add(this.openButtonParts);
             this.Controls.Add(this.openButtonDron);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.textBoxTech);
+            this.Controls.Add(this.textBoxParts);
+            this.Controls.Add(this.textBoxDron);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -193,14 +164,14 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxDron;
+        private System.Windows.Forms.TextBox textBoxParts;
+        private System.Windows.Forms.TextBox textBoxTech;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.Button closeButton;
         private System.Windows.Forms.Button openButtonDron;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button openButtonParts;
+        private System.Windows.Forms.Button openButtonTech;
     }
 }
 
