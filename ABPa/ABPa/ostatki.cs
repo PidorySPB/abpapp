27d2c7522ea3f5ceec686b73c49bbe7146617a63
nikit <nikita2000909@gmail.com>
2020-03-26 13:30:11 +0300

    Ну всё, приехали

--- ABPa/ABPa/ostatki.cs
+++ ABPa/ABPa/ostatki.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ABPa
 {
@@ -19,8 +21,50 @@ namespace ABPa
             dateTimePicker1.MaxDate = thisDay;
             dateTimePicker1.Value = thisDay;
 
+            LoadData();
         }
 
+        private void LoadData()
+        {
+
+            var dbCon = DBConn.Instance();
+            dbCon.DatabaseName = "570_abp";
+            dbCon.Host = "5.187.7.31";
+            string query = "";
+            if (dbCon.IsConnect())
+            {
+
+
+                query = String.Format("SELECT name, SUM(count) FROM `priem` Where date = '26.03.2020' GROUP BY name");
+
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                cmd.ExecuteNonQuery();
+
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+
+                List<string[]> data = new List<string[]>();
+
+                while (reader.Read())
+                {
+                    data.Add(new string[3]);
+
+
+                    data[data.Count - 1][1] = reader[1].ToString();
+                    data[data.Count - 1][2] = reader[2].ToString();
+                }
+
+                reader.Close();
+
+
+                dbCon.Close();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                foreach (string[] s in data)
+                    dataGridViewOst.Rows.Add(s);
+            }
+        }
         private void close_Click(object sender, EventArgs e)
         {
             this.Close();
